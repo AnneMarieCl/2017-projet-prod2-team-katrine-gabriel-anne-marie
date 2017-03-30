e663809e5a356014d4552b375a7c2bb5b388d3fa
mercurek <katmercu@hotmail.com>
2017-03-30 10:52:39 -0400

    iframe = fullwidth

--- src/www/a-propos.php
+++ src/www/a-propos.php
@@ -5,7 +5,6 @@
         <!-- Main -->
         <main>
 
-
             <!-- Section : Présentation de la chambre -->
             <section id="presentation-chambre">
                 <div class="container-fluid">
@@ -13,9 +12,7 @@
                         <h3 class="col-xs-12 col-sm-12 col-md-12 col-lg-12">Présentation de la chambre</h3>
 
                         <!-- Intégration Google Maps -->
-                      
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sfr!2sus!4v1490800428198!6m8!1m7!1sGhuAfBcIWMAAAAQWnf1Wwg!2m2!1d46.52240245695442!2d-72.7484868765236!3f66.7892009827807!4f-3.9025932898902482!5f0.4000000000000002"></iframe>
-                       
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sfr!2sus!4v1490800428198!6m8!1m7!1sGhuAfBcIWMAAAAQWnf1Wwg!2m2!1d46.52240245695442!2d-72.7484868765236!3f66.7892009827807!4f-3.9025932898902482!5f0.4000000000000002"style="border: 0; width: 100%; height: 100%"></iframe>
 
                         <!-- Information générales sur la CCIS -->
                         <h4>La CCIS est un réseau structuré de plus de 600 gens d'affaires représentant la communauté d'affaires de Shawinigan et qui a pour mandat d'initier des actions concertées favorisant l'avancement économique de son territoire. Le territoire desservi par la CCIS est : la Ville de Shawinigan (secteurs Grand-Mère, Lac-à-la-Tortue, Saint-Georges-de-Champlain, Saint-Gérard-des-Laurentides, Shawinigan et Shawinigan-Sud), ainsi que les municipalités de Charrette, Saint-Boniface, Notre-Dame-du-Mont-Carmel et Saint-Mathieu-du-Parc.<br></h4>
