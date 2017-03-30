3140d72336e9b243f92d34671f2c1f18048289bd
gabbaril8 <gab_baril@hotmail.com>
2017-03-30 13:31:54 -0400

    update blogue

--- src/www/blogue.php
+++ src/www/blogue.php
@@ -27,6 +27,152 @@
                                         </ul>
                                     </div>
 
+                                 </div>
+
+                                 <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1">
+
+                                    <ul class="pager">
+			
+                                        <li><a class="icon-prev" href=""><span class="fa fa-angle-double-left"></span></a></li>                           
+                                        <li><a class="page-nbr current" href="?page=1">1</a></li>                               
+                                        <li><a class="page-nbr" href="?page=2">2</a></li>                                   
+                                        <li><a class="page-nbr" href="?page=3">3</a></li>
+                                        <li><a class="page-nbr" href="?page=4">4</a></li>
+                                        <li><a class="page-nbr" href="?page=5">5</a></li>
+                                        <li><a class="page-nbr" href="?page=6">6</a></li>
+                                        <li><a class="icon-next" href=""><span class="fa fa-angle-double-right"></span></a></li>
+                                            
+                                    </ul>
+	    
+                                </div>
+
+                                <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1">
+                                    
+                                    <div class="article">
+                                        <div class="row">
+                                        
+                                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                                <img src="assets/images/blogue.jpg" class="img-responsive" alt="">
+                                                <span class="ruban pull-left">Catégorie</span>
+                                            </div>
+                                        
+                                            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+
+                                                <h4>À l’aube du transport intelligent au Québec</h4>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate [...]</p>
+                                                <a href="">Lire la suite</a>
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="article">
+                                        <div class="row">
+                                        
+                                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                                <img src="assets/images/blogue.jpg" class="img-responsive" alt="">
+                                                <span class="ruban">Catégorie</span>
+                                            </div>
+                                        
+                                            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+
+                                                <h4>À l’aube du transport intelligent au Québec</h4>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate [...]</p>
+                                                <a href="">Lire la suite</a>
+
+                                            </div>
+                                        
+                                        </div>
+                                    </div>
+                                    <div class="article">
+                                        <div class="row">
+                                        
+                                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                                <img src="assets/images/blogue.jpg" class="img-responsive" alt="">
+                                                <span class="ruban">Catégorie</span>
+                                            </div>
+                                        
+                                            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+
+                                                <h4>À l’aube du transport intelligent au Québec</h4>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate [...]</p>
+                                                <a href="">Lire la suite</a>
+
+                                            </div>
+                                        
+                                        </div>
+                                    </div>
+                                    <div class="article">
+                                        <div class="row">
+                                        
+                                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                                <img src="assets/images/blogue.jpg" class="img-responsive" alt="">
+                                                <span class="ruban">Catégorie</span>
+                                            </div>
+                                        
+                                            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+
+                                                <h4>À l’aube du transport intelligent au Québec</h4>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate [...]</p>
+                                                <a href="">Lire la suite</a>
+
+                                            </div>
+                                        
+                                        </div>
+                                    </div>
+                                    <div class="article">
+                                        <div class="row">
+                                        
+                                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                                <img src="assets/images/blogue.jpg" class="img-responsive" alt="">
+                                                <span class="ruban">Catégorie</span>
+                                            </div>
+                                        
+                                            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+
+                                                <h4>À l’aube du transport intelligent au Québec</h4>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate [...]</p>
+                                                <a href="">Lire la suite</a>
+
+                                            </div>
+                                        
+                                        </div>
+                                    </div>
+                                    <div class="article">
+                                        <div class="row">
+                                        
+                                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                                <img src="assets/images/blogue.jpg" class="img-responsive" alt="">
+                                                <span class="ruban">Catégorie</span>
+                                            </div>
+                                        
+                                            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+
+                                                <h4>À l’aube du transport intelligent au Québec</h4>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate [...]</p>
+                                                <a href="">Lire la suite</a>
+
+                                            </div>
+                                        
+                                        </div>
+                                    </div>
+	    
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1">
+
+                                    <ul class="pager">
+			
+                                        <li><a class="icon-prev" href=""><span class="fa fa-angle-double-left"></span></a></li>                           
+                                        <li><a class="page-nbr current" href="?page=1">1</a></li>                               
+                                        <li><a class="page-nbr" href="?page=2">2</a></li>                                   
+                                        <li><a class="page-nbr" href="?page=3">3</a></li>
+                                        <li><a class="page-nbr" href="?page=4">4</a></li>
+                                        <li><a class="page-nbr" href="?page=5">5</a></li>
+                                        <li><a class="page-nbr" href="?page=6">6</a></li>
+                                        <li><a class="icon-next" href=""><span class="fa fa-angle-double-right"></span></a></li>
+                                            
+                                    </ul>
+	    
                                 </div>
                                 
                             </div>
