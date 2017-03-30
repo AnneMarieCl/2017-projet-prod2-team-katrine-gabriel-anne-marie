e471bbdbd0a5de47492b1ceebe417ee70bc66953
Katrine Mercure <katmercu@hotmail.com>
2017-03-30 19:01:57 -0400

    Style du footer comme celui du header!!!

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -5,7 +5,7 @@
                 <div class="container-fluid">
                     <div class="row">
                         <!-- Logo de l'entreprise -->
-                        <div id="img-footer" class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
+                        <div id="img-footer" class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
                             <a href="index.php"><img src="assets/images/logo-footer.png" alt="Logo CCIS"></a>
                         </div>
 
@@ -22,7 +22,7 @@
                         </div>
 
                         <!-- Infolettre -->
-                        <div id="infolettre" class="col-xs-12 col-sm-3 col-md-3 col-lg-4">
+                        <div id="infolettre" class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
                             <h5>INFOLETTRE</h5>
                             <p>Restez informés, inscrivez-vous à notre infolettre</p>
                             <form id="form-infolettre">
