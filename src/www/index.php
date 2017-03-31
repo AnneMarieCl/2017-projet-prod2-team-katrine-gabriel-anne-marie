d272ab85d4a4ea42986f739dfa20ae2f4d31b776
Anne-Marie <anne-marie.cloutier@hotmail.com>
2017-03-31 09:41:28 -0400

    avancement intégration page d'accueil

--- src/www/index.php
+++ src/www/index.php
@@ -16,32 +16,95 @@
                                 <p>Blogue</p>
                                 <h2>Shawinigan, un modèle en matière de développement des régions</h2>
                                 <p>Catégorie</p>
-                                <span><p>En savoir plus</p></span>
+                                <a href="#"><p class="savoir-plus">En savoir plus</p></a>
                             </div> 
                         </div>
                 </section>
                 <section class="evenement">
                     <div class="row">
-                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 texte">
                             <p>Événement</p>
                             <h3>Toute la ville parle PME</h3>
                             <p>Les entrepreneurs d’ici sont inspirants. Venez découvrir leur parcours tout au long du développement de leur entreprise [...]</p>
-                            <p>S'inscrire</P>
+                            <a href="#"><p class="inscription">S'inscrire</p></a>
                         </div>
-                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                            
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 image">
+                            <img src="https://dummyimage.com/400.png/455560/fff" class="img-responsive" alt=""><a href="#"></a>
+                            <span class="ruban pull-left">Catégorie</span>
                         </div>
-                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 texte">
                             <p>Événement</p>
                             <h3>Défis des entreprises</h3>
                             <p>Venez vivre en grand nombre une expérience sportive unique sur l’île Melville.</p>
-                            <p>S'inscrire</P>
+                            <a href="#"><p class="inscription2">S'inscrire</p></a>
                         </div>
-                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                            
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 image">
+                             <img src="https://dummyimage.com/400.png/455560/fff" class="img-responsive" alt=""><a href="#"></a>
+                             <span class="ruban pull-left">Catégorie</span>
                         </div>
                     </div>
-                
+                </section>
+                <section class="publication">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 titre">
+                            <h4>Publication</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3 image">
+                             <img src="https://dummyimage.com/325.png/455560/fff" class="img-responsive" alt=""><a href="#"></a>
+                             <span class="ruban pull-left">Catégorie</span>
+                        </div>
+                        <div class="col-xs-12 col-sm-8 col-md-9 col-lg-9 texte">
+                            <h3>Formulaire de candidature <span>ca ccis</span> 2017-2018</h3>
+                            <p>Remplissez un formulaire et déposer votre candidature comme administrateur de la CCIS.</p>
+                            <a href="#"><p class="lien">Lire la suite</p></a>
+                            <a href="#"><p class="lien">Allez aux publications</p></a>
+                        </div>
+                    </div>
+                     <div class="row">
+                        <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3 image">
+                             <img src="https://dummyimage.com/325.png/455560/fff" class="img-responsive" alt=""><a href="#"></a>
+                             <span class="ruban pull-left">Catégorie</span>
+                        </div>
+                        <div class="col-xs-12 col-sm-8 col-md-9 col-lg-9 texte">
+                            <h3>Formulaire de candidature <span>ca ccis</span> 2017-2018</h3>
+                            <p>Remplissez un formulaire et déposer votre candidature comme administrateur de la CCIS.</p>
+                            <a href="#"><p class="lien">Lire la suite</p></a>
+                            <a href="#"><p class="lien">Allez aux publications</p></a>
+                        </div>
+                    </div>
+                </section>
+                <section class="blogue">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 titre">
+                            <h4>Blogue</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3 image">
+                             <img src="https://dummyimage.com/325.png/455560/fff" class="img-responsive" alt=""><a href="#"></a>
+                             <span class="ruban pull-left">Catégorie</span>
+                        </div>
+                        <div class="col-xs-12 col-sm-8 col-md-9 col-lg-9 texte">
+                            <h3>Formulaire de candidature <span>ca ccis</span> 2017-2018</h3>
+                            <p>Remplissez un formulaire et déposer votre candidature comme administrateur de la CCIS.</p>
+                            <a href="#"><p class="lien">Lire la suite</p></a>
+                            <a href="#"><p class="lien">Allez aux publications</p></a>
+                        </div>
+                    </div>
+                     <div class="row">
+                        <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3 image">
+                             <img src="https://dummyimage.com/325.png/455560/fff" class="img-responsive" alt=""><a href="#"></a>
+                             <span class="ruban pull-left">Catégorie</span>
+                        </div>
+                        <div class="col-xs-12 col-sm-8 col-md-9 col-lg-9 texte">
+                            <h3>Formulaire de candidature <span>ca ccis</span> 2017-2018</h3>
+                            <p>Remplissez un formulaire et déposer votre candidature comme administrateur de la CCIS.</p>
+                            <a href="#"><p class="lien">Lire la suite</p></a>
+                            <a href="#"><p class="lien">Allez aux publications</p></a>
+                        </div>
+                    </div>
                 </section>
             </div>
 
