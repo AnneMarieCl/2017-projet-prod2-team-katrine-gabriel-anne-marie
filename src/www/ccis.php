dee34a66bde81f991a49e6e22c1ef4f2ba72db01
gabbaril8 <gab_baril@hotmail.com>
2017-03-31 14:54:00 -0400

    sidebar à propos

--- src/www/ccis.php
+++ src/www/ccis.php
@@ -5,6 +5,10 @@
         
         <header>
             <?php include'../includes/nav.php'; ?>
+            <!-- Image -->
+            <div class="background-image">
+                <img src="assets/images/background-header.jpg" alt="CCIS">
+            </div>
         </header>
 
         <!-- Main -->
@@ -13,69 +17,105 @@
             <!-- Section : Présentation de la chambre -->
             <section id="presentation-chambre">
                 <div class="container-fluid">
-
-                    <!-- Image -->
-                    <div class="background-image">
-                        <img src="assets/images/background-header.jpg" alt="CCIS">
-                    </div>
-
+                    
                     <div class="row">
-                        <h3 class="col-xs-12 col-sm-8 col-md-8 col-lg-8">Présentation de la chambre</h3>
+                        
+                        <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
 
-                        <!-- Intégration Google Maps -->
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sfr!2sus!4v1490800428198!6m8!1m7!1sGhuAfBcIWMAAAAQWnf1Wwg!2m2!1d46.52240245695442!2d-72.7484868765236!3f66.7892009827807!4f-3.9025932898902482!5f0.4000000000000002" style="border: 0; width: 60%; height: 100%"></iframe>
+                            <div class="row">
+                                <h3 class="col-xs-12 col-sm-8 col-md-8 col-lg-8">Présentation de la chambre</h3>
 
-                        <!-- Information générales sur la CCIS -->
-                        <p class="col-xs-12 col-sm-12 col-md-12 col-lg-12 gras">La CCIS est un réseau structuré de plus de 600 gens d'affaires représentant la communauté d'affaires de Shawinigan et qui a pour mandat d'initier des actions concertées favorisant l'avancement économique de son territoire. Le territoire desservi par la CCIS est : la Ville de Shawinigan (secteurs Grand-Mère, Lac-à-la-Tortue, Saint-Georges-de-Champlain, Saint-Gérard-des-Laurentides, Shawinigan et Shawinigan-Sud), ainsi que les municipalités de Charrette, Saint-Boniface, Notre-Dame-du-Mont-Carmel et Saint-Mathieu-du-Parc.<br></p>
-                        <p>La Chambre met en valeur le succès économique régional des entreprises et des gens d’affaires de la région. Elle intervient de façon vigilante et constructive auprès des instances et autorités afin de défendre ses membres. Elle stimule le réseautage de ses membres et développe les compétences des gens d’affaires en offrant de l’information, de l’expertise es et en les aiguillant vers les ressources locales.<p>
+                                <!-- Intégration Google Maps -->
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sfr!2sus!4v1490800428198!6m8!1m7!1sGhuAfBcIWMAAAAQWnf1Wwg!2m2!1d46.52240245695442!2d-72.7484868765236!3f66.7892009827807!4f-3.9025932898902482!5f0.4000000000000002" style="border: 0; width: 60%; height: 100%"></iframe>
 
-                        <p><b>La Chambre de commerce et d'industrie de Shawinigan :</b></p>
-                        <ul>
-                            <li>Est la voix de la communauté d'affaires</li>
-                            <li>Est reconnue comme le plus important mouvement d'affaires de la région</li>
-                            <li>Possède de l'expertise au service des gens d'affaires</li>
-                            <li>Rassemble tous les secteurs d'activités économiques et socio-économiques de la région</li>
-                            <li>Favorise la promotion de l'activité commerciale</li>
-                            <li>Crée de multiples occasions de réseautage d'affaires</li>
-                            <li>Stimule et dynamise le milieu économique par ses initiatives</li>
-                        </ul>
+                                <!-- Information générales sur la CCIS -->
+                                <p class="col-xs-12 col-sm-12 col-md-12 col-lg-12 gras">La CCIS est un réseau structuré de plus de 600 gens d'affaires représentant la communauté d'affaires de Shawinigan et qui a pour mandat d'initier des actions concertées favorisant l'avancement économique de son territoire. Le territoire desservi par la CCIS est : la Ville de Shawinigan (secteurs Grand-Mère, Lac-à-la-Tortue, Saint-Georges-de-Champlain, Saint-Gérard-des-Laurentides, Shawinigan et Shawinigan-Sud), ainsi que les municipalités de Charrette, Saint-Boniface, Notre-Dame-du-Mont-Carmel et Saint-Mathieu-du-Parc.<br></p>
+                                <p>La Chambre met en valeur le succès économique régional des entreprises et des gens d’affaires de la région. Elle intervient de façon vigilante et constructive auprès des instances et autorités afin de défendre ses membres. Elle stimule le réseautage de ses membres et développe les compétences des gens d’affaires en offrant de l’information, de l’expertise es et en les aiguillant vers les ressources locales.<p>
 
-                        <p><b>La Chambre de commerce et d'industrie de Shawinigan, c'est :</b></p>
-                        <ul>
-                            <li>Un conseil d'administration dévoué</li>
-                            <li>Une équipe dynamique à la permanence</li>
-                            <li>Une mission économique et des valeurs organisationnelles </li>
-                            <li>Une implication sociale au sein de nombreux comités </li>
-                        </ul>
+                                <p><b>La Chambre de commerce et d'industrie de Shawinigan :</b></p>
+                                <ul>
+                                    <li>Est la voix de la communauté d'affaires</li>
+                                    <li>Est reconnue comme le plus important mouvement d'affaires de la région</li>
+                                    <li>Possède de l'expertise au service des gens d'affaires</li>
+                                    <li>Rassemble tous les secteurs d'activités économiques et socio-économiques de la région</li>
+                                    <li>Favorise la promotion de l'activité commerciale</li>
+                                    <li>Crée de multiples occasions de réseautage d'affaires</li>
+                                    <li>Stimule et dynamise le milieu économique par ses initiatives</li>
+                                </ul>
 
-                        <!-- Historique -->
-                        <h3>Historique</h3>
-                        <p><b>À l'origine, trois chambres de commerce &#339;uvraient sur l'ensemble du territoire du Centre-de-la-Mauricie.</b></p>
-                        <p>Grâce à la fusion des chambres de Shawinigan, de Grand-Mère et de St-Boniface, la Chambre de commerce du Centre-de-la-Mauricie a vu le jour en 2000. 
-                            Cette initiative était une première pour doter un territoire aussi grand qu'une MRC d'une Chambre régionale de cette ampleur. Depuis, la Chambre de commerce
-                            du Centre-de-la-Mauricie a su s'investir dans de nombreux projets socioéconomiques et devenir un acteur important au sein de la région. En 2007, la Chambre 
-                            de commerce du Centre-de-la-Mauricie changeait son appellation pour la Chambre de commerce et d'industrie de Shawinigan.</p>
+                                <p><b>La Chambre de commerce et d'industrie de Shawinigan, c'est :</b></p>
+                                <ul>
+                                    <li>Un conseil d'administration dévoué</li>
+                                    <li>Une équipe dynamique à la permanence</li>
+                                    <li>Une mission économique et des valeurs organisationnelles </li>
+                                    <li>Une implication sociale au sein de nombreux comités </li>
+                                </ul>
 
-                        <!-- Mission -->
-                        <h3>Mission</h3>
-                        <p>Être un acteur incontournable dans le développement économique de Shawinigan et de la Mauricie.</p>
+                                <!-- Historique -->
+                                <h3>Historique</h3>
+                                <p><b>À l'origine, trois chambres de commerce &#339;uvraient sur l'ensemble du territoire du Centre-de-la-Mauricie.</b></p>
+                                <p>Grâce à la fusion des chambres de Shawinigan, de Grand-Mère et de St-Boniface, la Chambre de commerce du Centre-de-la-Mauricie a vu le jour en 2000. 
+                                    Cette initiative était une première pour doter un territoire aussi grand qu'une MRC d'une Chambre régionale de cette ampleur. Depuis, la Chambre de commerce
+                                    du Centre-de-la-Mauricie a su s'investir dans de nombreux projets socioéconomiques et devenir un acteur important au sein de la région. En 2007, la Chambre 
+                                    de commerce du Centre-de-la-Mauricie changeait son appellation pour la Chambre de commerce et d'industrie de Shawinigan.</p>
 
-                        <!-- Objectifs -->
-                        <h3>Objectifs</h3>
-                        <ul>
-                            <li>Agir pour la prospérité de nos membres en favorisant la croissance économique, le développement durable et la responsabilité sociale de notre région.</li>
-                            <li>Stimuler le réseautage de nos membres.</li>
-                            <li>Informer et soutenir la formation des gens d'affaires.</li>
-                            <li>Intervenir auprès des différentes instances afin de mieux défendre les intérêts de nos membres.</li>
-                            <li>Cultiver l'entrepreneuriat pour diversifier notre économie.</li>
-                        </ul>
+                                <!-- Mission -->
+                                <h3>Mission</h3>
+                                <p>Être un acteur incontournable dans le développement économique de Shawinigan et de la Mauricie.</p>
 
-                        <!-- Section : Devenez membre -->
-                        <img class="" src="assets/images/avatar-membre.png" alt="Devenez membre">
-                        <p class="">DEVENEZ MEMBRE<p>
-                        <p class="">Solidifiez vos acquis et accédez à une clientèle privilège!</p>
-                        <a class="" href="">En savoir plus</a>
+                                <!-- Objectifs -->
+                                <h3>Objectifs</h3>
+                                <ul>
+                                    <li>Agir pour la prospérité de nos membres en favorisant la croissance économique, le développement durable et la responsabilité sociale de notre région.</li>
+                                    <li>Stimuler le réseautage de nos membres.</li>
+                                    <li>Informer et soutenir la formation des gens d'affaires.</li>
+                                    <li>Intervenir auprès des différentes instances afin de mieux défendre les intérêts de nos membres.</li>
+                                    <li>Cultiver l'entrepreneuriat pour diversifier notre économie.</li>
+                                </ul>
+
+                                <!-- Section : Devenez membre -->
+                                <img class="" src="assets/images/avatar-membre.png" alt="Devenez membre">
+                                <p class="">DEVENEZ MEMBRE<p>
+                                <p class="">Solidifiez vos acquis et accédez à une clientèle privilège!</p>
+                                <a class="" href="">En savoir plus</a>
+                            </div>
+                        </div>
+                        
+                        <aside class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                            
+                            <div class="row">
+                                
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <div id="membre" class="sidebar">
+                                        <h4><img src="https://dummyimage.com/28x28/000/fff.jpg" alt="">DEVENEZ MEMBRE</h4>
+                                        <p>Solidifier vos acquis et  accédez à une clientèle privilèges</p>
+                                        <a href="" class="text-center">Devenir membre</a>
+                                    </div>
+                                    <div id="sms" class="sidebar">
+                                        <h4><img src="https://dummyimage.com/28x28/000/fff.jpg" alt="">NE RATEZ AUCUNE ACTIVITÉ</h4>
+                                        <p>Inscrivez-vous à l’alerte SMS !</p>
+                                        <a href="" class="text-center">S'inscrire</a>
+                                    </div>
+                                    <div id="membre" class="sidebar">
+                                        <h4><img src="https://dummyimage.com/28x28/000/fff.jpg" alt="">TOUTE LA VILLE PARLE PME</h4>
+                                        <p>Les entrepreneurs d’ici sont inspirants. Venez découvrir leur parcours tout au long du développement de leur entreprise...</p>
+                                        <a href="" class="text-center">S'inscrire</a>
+                                    </div>
+                                    <div id="membre" class="sidebar">
+                                        <h4><img src="https://dummyimage.com/28x28/000/fff.jpg" alt="">DÉFI DES ENTREPRISES</h4>
+                                        <p>Venez vivre en grand nombre une expérience sportive unique sur l’île Melville.</p>
+                                        <a href="" class="text-center">S'inscrire</a>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            
+                        </aside>
+                        
+                        
                     </div>
+                    
+                    
                 </div>
             </section>   
         </main>
