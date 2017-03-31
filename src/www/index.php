97c03a892657fb6102bdc2687ca6f148b75744ba
Anne-Marie <anne-marie.cloutier@hotmail.com>
2017-03-31 06:54:29 -0400

    visuel du slider et transparence du menu déroulant

--- src/www/index.php
+++ src/www/index.php
@@ -8,6 +8,42 @@
         </header>
 
         <main>
+             <div class="container-fluid">
+                <section class="slider">
+                    <div class="image-1">
+                            <img class="un" src="assets/images/slider-1.jpg" alt="Chania">
+                            <div class="carousel-caption">
+                                <p>Blogue</p>
+                                <h2>Shawinigan, un modèle en matière de développement des régions</h2>
+                                <p>Catégorie</p>
+                                <span><p>En savoir plus</p></span>
+                            </div> 
+                        </div>
+                </section>
+                <section class="evenement">
+                    <div class="row">
+                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                            <p>Événement</p>
+                            <h3>Toute la ville parle PME</h3>
+                            <p>Les entrepreneurs d’ici sont inspirants. Venez découvrir leur parcours tout au long du développement de leur entreprise [...]</p>
+                            <p>S'inscrire</P>
+                        </div>
+                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                            
+                        </div>
+                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                            <p>Événement</p>
+                            <h3>Défis des entreprises</h3>
+                            <p>Venez vivre en grand nombre une expérience sportive unique sur l’île Melville.</p>
+                            <p>S'inscrire</P>
+                        </div>
+                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                            
+                        </div>
+                    </div>
+                
+                </section>
+            </div>
 
         </main>
 
