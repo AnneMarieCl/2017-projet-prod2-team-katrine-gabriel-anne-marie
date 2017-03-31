31862bf06a9c0956d70519611f04c6acf8778166
Anne-Marie <anne-marie.cloutier@hotmail.com>
2017-03-31 11:52:24 -0400

    avancement intégration page accueil

--- src/www/index.php
+++ src/www/index.php
@@ -18,7 +18,7 @@
                                 <p>Catégorie</p>
                                 <a href="#"><p class="savoir-plus">En savoir plus</p></a>
                             </div> 
-                        </div>
+                    </div>
                 </section>
                 <section class="evenement">
                     <div class="row">
@@ -96,7 +96,7 @@
                             <h2>Formulaire de candidature <span>ca ccis</span> 2017-2018</h2>
                             <p>Remplissez un formulaire et déposer votre candidature comme administrateur de la CCIS.</p>
                             <a href="#"><p class="lien">Lire la suite</p></a>
-                            <a href="#"><p class="lien">Allez aux publications</p></a>
+                            <a href="#"><p class="lien">Allez sur le blogue</p></a>
                         </div>
                         <div class="col-xs-hidden col-sm-hidden col-md-2 col-lg-3 texte">
                             
@@ -111,13 +111,43 @@
                             <h2>Formulaire de candidature <span>ca ccis</span> 2017-2018</h2>
                             <p>Remplissez un formulaire et déposer votre candidature comme administrateur de la CCIS.</p>
                             <a href="#"><p class="lien">Lire la suite</p></a>
-                            <a href="#"><p class="lien">Allez aux publications</p></a>
+                            <a href="#"><p class="lien">Allez sur le blogue</p></a>
                         </div>
                         <div class="col-xs-hidden col-sm-hidden col-md-2 col-lg-3 texte">
                             
                         </div>
                     </div>
                 </section>
+               
+                    <div class="row">
+                        <section class="devenir-membre">
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                <div class="image-1">
+                                    <img class="fond" src="assets/images/background-membre.png" alt="Chania">
+                                    <div class="appel-action"> 
+                                        <div class="row">
+                                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                                <h2>Vous désirez vous joindre à nous?</h2>
+                                            </div>
+                                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                                
+                                            </div>
+                                        </div>
+                                        <div class"row">
+                                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 bouton-gauche">
+                                                <a class="bouton" href="#"><h3>Devenez membre !</h3>
+                                                <p>Solidifier vos acquis et  accédez à une clientèle privilège</p></a>
+                                            </div>
+                                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 bouton-droit">
+                                                <a class="bouton" href="#"><h3>Ne ratez aucune activité !</h3>
+                                                <p>Inscrivez-vous à l’alerte <span>SMS</span></p></a>
+                                            </div>
+                                        </div>    
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
             </div>
 
         </main>
