ff0425a76236b099c30ee87e9b9c0a6a57ae6c08
Katrine Mercure <katmercu@hotmail.com>
2017-04-03 19:08:04 -0400

    footer.php done

--- src/www/travailfinal/wp-content/themes/ccistheme/footer.php
+++ src/www/travailfinal/wp-content/themes/ccistheme/footer.php
@@ -59,3 +59,8 @@
             </section>
         </footer>
         <!-- Fin du footer-->
+
+        <!-- jQuery -->
+        <script src="//code.jquery.com/jquery.js"></script>
+        <!-- Bootstrap JavaScript -->
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
