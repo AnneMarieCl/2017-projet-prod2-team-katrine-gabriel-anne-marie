60378844e64eca6d8e9c0ca1fe2139c702fe31dc
Katrine Mercure <katmercu@hotmail.com>
2017-04-03 19:15:35 -0400

    début des liens wp

--- src/www/travailfinal/wp-content/themes/ccistheme/header.php
+++ src/www/travailfinal/wp-content/themes/ccistheme/header.php
@@ -5,12 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <title>CCIS</title>
         <!-- BootStrap -->
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?php echo get_template_directory_uri(); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Lato%7CTauri" rel="stylesheet">
         <script src="https://use.fontawesome.com/2a85d3823f.js"></script>
         <!-- Style.css -->
-        <link href="assets/css/main.css" rel="stylesheet">
+        <link href="<?php bloginfo("stylesheet_url") ?>" rel="stylesheet">
     </head>
     <body>
         <header>
