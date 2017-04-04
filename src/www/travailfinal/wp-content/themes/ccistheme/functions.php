05decb606b9efc7c2ce722377f11a215ac380f3e
gabbaril8 <gab_baril@hotmail.com>
2017-04-04 11:21:54 -0400

    update thumbnail

--- src/www/travailfinal/wp-content/themes/ccistheme/functions.php
+++ src/www/travailfinal/wp-content/themes/ccistheme/functions.php
@@ -11,5 +11,8 @@ function mesMenus() {
 // Déclencher les fonctions
 add_action( 'init', 'mesMenus' );
 
+// Add Thumbnail
+add_theme_support( 'post-thumbnails' );
+add_image_size( 'blogue', '', 800, true );
 
 ?>
