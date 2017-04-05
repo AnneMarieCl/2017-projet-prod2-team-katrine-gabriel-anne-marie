163f93f9d38a581a6eb71e74d3fe09e27953949d
Katrine Mercure <katmercu@hotmail.com>
2017-04-05 09:45:38 -0400

    caller les titres de wordpress

--- src/www/travailfinal/wp-content/themes/ccistheme/page-conseil-administration.php
+++ src/www/travailfinal/wp-content/themes/ccistheme/page-conseil-administration.php
@@ -5,7 +5,7 @@
             <!-- Image -->
             <div class="background-img hidden-xs" id="header">
                 <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/background-header.jpg" alt="CCIS">
-                <h1>CONSEIL D'ADMINISTRATION</h1>
+                <h1><?php the_title();?></h1>
             </div>
 
             <!-- Section : Conseil d'administration -->
--- src/www/travailfinal/wp-content/themes/ccistheme/page-espace-membre.php
+++ src/www/travailfinal/wp-content/themes/ccistheme/page-espace-membre.php
@@ -5,7 +5,7 @@
             <!-- Image -->
             <div class="background-img hidden-xs" id="header">
                 <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/background-membre.png" alt="L'espace membre">
-                <h1>ESPACE MEMBRE</h1>
+                <h1><?php the_title();?></h1>
             </div>
 
             <!-- Section : Espace membre  -->
