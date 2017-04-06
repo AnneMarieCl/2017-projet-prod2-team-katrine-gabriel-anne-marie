0d24002617b7376ef2ce73df411838aacc91f7ba
Katrine Mercure <katmercu@hotmail.com>
2017-04-06 17:36:39 -0400

    css...

--- src/www/travailfinal/wp-content/themes/ccistheme/page-blogue.php
+++ src/www/travailfinal/wp-content/themes/ccistheme/page-blogue.php
@@ -13,7 +13,7 @@
                     
                     <div class="row">
                         
-                        <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+                        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-9">
 
                             
                             <div class="row">
@@ -140,18 +140,14 @@
                             
                         </div>
                         
-                        <aside class="col-xs-0 col-sm-3 col-md-3 col-lg-3">
-                            
-                            <div class="row">
-                                
-                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                                    <?php if ( !function_exists('dynamic_sidebar')|| !dynamic_sidebar( 'sidebar-blogue' ) ) : Endif; ?>
-                                </div>
-                                
-                            </div>
-                            
-                        </aside>
-                        
+
+                        <!-- Sidebar -->
+                        <aside class="hidden-xs col-sm-4 col-md-4 col-lg-3 formation">
+                            <ul class="row">
+                                <!-- Côté droit -->
+                                 <?php if ( !function_exists('dynamic_sidebar')|| !dynamic_sidebar( 'sidebar-blogue' ) ) : Endif; ?>
+                            </ul>
+                        </aside>    
                         
                     </div>
                     
