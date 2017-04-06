5b855f1069f716fb17cd4b329453000abfdc990e
Katrine Mercure <katmercu@hotmail.com>
2017-04-06 15:55:36 -0400

    style de l'article :)!

--- src/www/travailfinal/wp-content/themes/ccistheme/single.php
+++ src/www/travailfinal/wp-content/themes/ccistheme/single.php
@@ -15,55 +15,57 @@
 
                 
                 <div class="row">
-                    
-                    <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1">
+    
+                    <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1 test">
                         
                         <span class="ruban pull-left"><?php the_category(); ?></span>
                         <h2><?php the_title(); ?></h2>
                         <?php $author = get_the_author(); ?> 
-                        <span><i class="fa fa-user"></i> Écrit par <a href="#"><?php the_author();?></a></span>
-                        <h5 id="publish_date">Publié le <?php the_date(); ?><h5>
-        
-                                <?php
-                                if (has_post_thumbnail()){
-                                    the_post_thumbnail('blogue', array('class' => 'col-md-6 img-responsive'));
-                                }
-                                else {
-                                    echo '<img src="'.get_template_directory_uri().'/assets/images/thumbnail-ccis.jpg" alt="" class="img-responsive">';
-                                }
-                                the_content();
-                                ?>
+                        <span>Écrit par <?php the_author();?></span>
+                        <h6 id="publish_date">Publié le <?php the_date(); ?><h6>
                         
-                        <span class="comments-made"><i class="fa fa-comment"></i> <a href="#">1 commentaire</a></span>
-                        <h2>Commentaires</h2>
+                        <?php
+                            if (has_post_thumbnail()){
+                                the_post_thumbnail('blogue', array('class' => 'col-md-6 img-responsive'));
+                            }
+                            else {
+                                echo '<img src="'.get_template_directory_uri().'/assets/images/thumbnail-ccis.jpg" alt="" class="img-responsive">';
+                            }
+                            the_content();
+                        ?>
+                    </div>
+                        
 
-                        <div class="comments">
-                            <form role="form">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" id="name" placeholder="Votre nom">
-                                </div>
+                    <div class="comments col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1">
+                        <form role="form">
+                            <h2>Commentaires</h2>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="name" placeholder="Votre nom">
+                            </div>
 
-                                <div class="form-group">
-                                    <input type="email" class="form-control" id="email" placeholder="Votre courriel">
-                                </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="email" placeholder="Votre courriel">
+                            </div>
 
-                                <div class="form-group">
-                                    <input type="text" class="form-control" id="website" placeholder="Votre site web">
-                                </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="website" placeholder="Votre site web">
+                            </div>
 
-                                <textarea class="form-control" rows="9" placeholder="Votre commentaire"></textarea>
-                                <button type="submit" class="btn btn-default">Commenter</button>
-                            </form>
-                        </div>
+                            <textarea class="form-control" rows="9" placeholder="Votre commentaire"></textarea>
+                            <button type="submit" class="btn btn-default">Commenter</button>
+                        </form>
+                    </div>
 
+                    <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1">
                         <nav>
                             <ul class="pager">
                                 <li class="previous"><a href="#"><span aria-hidden="true">&larr;</span> Article précédent</a></li>
                                 <li class="next pull-right"><a href="#">Article suivant <span aria-hidden="true">&rarr;</span></a></li>
                             </ul>
                         </nav>
-
                     </div>
+
+                    
                     
                 </div>
                 
