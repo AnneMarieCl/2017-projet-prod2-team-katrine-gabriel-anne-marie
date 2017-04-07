ce6e2c0bf89a36250a595c372546fe939405a5f9
Anne-Marie <anne-marie.cloutier@hotmail.com>
2017-04-07 11:06:58 -0400

    sidebar formations et événements aligné

--- src/www/travailfinal/wp-content/themes/ccistheme/page-formations-evenements.php
+++ src/www/travailfinal/wp-content/themes/ccistheme/page-formations-evenements.php
@@ -200,11 +200,11 @@
                             
                         </div>
                         
-                        <aside class="col-xs-0 col-sm-4 col-md-4 col-lg-3 formation padding-titre">
+                        <aside class="col-xs-0 col-sm-4 col-md-4 col-lg-3 formation">
                             
                             <div class="row">
                                 
-                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 padding-titre">
                                      <?php if ( !function_exists('dynamic_sidebar')|| !dynamic_sidebar( 'sidebar-events' ) ) : Endif; ?>
                                 </div>
                                 
