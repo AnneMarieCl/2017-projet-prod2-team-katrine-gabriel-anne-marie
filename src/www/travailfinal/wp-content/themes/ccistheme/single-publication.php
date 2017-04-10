84fff2529249ad96dbd96e421aa46f9302e6e594
gabbaril8 <gab_baril@hotmail.com>
2017-04-10 14:00:01 -0400

    single publication update

--- src/www/travailfinal/wp-content/themes/ccistheme/single-publication.php
+++ src/www/travailfinal/wp-content/themes/ccistheme/single-publication.php
@@ -2,7 +2,7 @@
 
 	<section id="blog-single" class="container-fluid">
         <div class="row">
-            <div class="col-md-9">
+            <div class="col-xs-12 col-sm-8 col-md-8 col-lg-9">
 
                     <?php 
                         if ( have_posts() ) {
@@ -15,55 +15,47 @@
 
                 
                 <div class="row">
-                    
-                    <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1">
+    
+                    <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1 test">
                         
-                        <span class="ruban pull-left"><?php the_category(); ?></span>
+                        <div class="ruban pull-left"><?php the_category(); ?></div>
                         <h2><?php the_title(); ?></h2>
                         <?php $author = get_the_author(); ?> 
-                        <span><i class="fa fa-user"></i> Écrit par <a href="#"><?php the_author();?></a></span>
-                        <h5 id="publish_date">Publié le <?php the_date(); ?><h5>
-        
-                                <?php
-                                if (has_post_thumbnail()){
-                                    the_post_thumbnail('blogue', array('class' => 'col-md-6 img-responsive'));
-                                }
-                                else {
-                                    echo '<img src="'.get_template_directory_uri().'/assets/images/thumbnail-ccis.jpg" alt="" class="img-responsive">';
-                                }
-                                the_content();
-                                ?>
+                        <span>Écrit par <?php the_author();?></span>
+                        <p id="publish_date">Publié le <?php the_date(); ?><p>
                         
-                        <span class="comments-made"><i class="fa fa-comment"></i> <a href="#">1 commentaire</a></span>
+                        <?php
+                            if (has_post_thumbnail()){
+                                the_post_thumbnail('blogue', array('class' => 'col-md-6 img-responsive'));
+                            }
+                            else {
+                                echo '<img src="'.get_template_directory_uri().'/assets/images/thumbnail-ccis.jpg" alt="" class="col-md-6 img-responsive">';
+                            }
+                            the_content();
 
-                        <h2>Commentaires</h2>
-                        <div class="comments">
-                            <form role="form">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" id="name" placeholder="Votre nom">
-                                </div>
+                            $file = get_field('piece_jointe');
 
-                                <div class="form-group">
-                                    <input type="email" class="form-control" id="email" placeholder="Votre courriel">
-                                </div>
+                            if( $file ): 
+                        
+                        ?>
+                                
+                                <a href="<?php echo $file['url']; ?>"><?php echo ('<h5>Télécharger le document</h5>'); ?></a>
 
-                                <div class="form-group">
-                                    <input type="text" class="form-control" id="website" placeholder="Votre site web">
-                                </div>
-
-                                <textarea class="form-control" rows="9" placeholder="Votre commentaire"></textarea>
-                                <button type="submit" class="btn btn-default">Commenter</button>
-                            </form>
-                        </div>
-
-                        <nav>
-                            <ul class="pager">
-                                <li class="previous"><a href="#"><span aria-hidden="true">&larr;</span> Article précédent</a></li>
-                                <li class="next pull-right"><a href="#">Article suivant <span aria-hidden="true">&rarr;</span></a></li>
-                            </ul>
-                        </nav>
-
+                            <?php endif; ?>
                     </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1">
+                        <nav>
+                            <div class="previous-article pull-left">
+                                <?php previous_post_link(); ?>
+                            </div>
+                            <div class="next-article pull-right">
+                                <?php next_post_link(); ?>
+                            </div>
+                        </nav>
+                    </div>
+
+                    
                     
                 </div>
                 
@@ -78,10 +70,18 @@
 
             </div><!--/.col-md-9-->
 
-            <aside class="col-md-3">
-
-            <?php if ( !function_exists('dynamic_sidebar')|| !dynamic_sidebar( 'sidebar-blogue' ) ) : Endif ?>
-
+            <aside class="col-xs-0 col-sm-4 col-md-4 col-lg-3 formation padding-titre">
+                
+                <div class="row">
+                    
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <ul>
+                            <?php if ( !function_exists('dynamic_sidebar')|| !dynamic_sidebar( 'sidebar-blogue' ) ) : Endif; ?>
+                        </ul>
+                    </div>
+                    
+                </div>
+                
             </aside>
 
         </div><!--/.row-->
