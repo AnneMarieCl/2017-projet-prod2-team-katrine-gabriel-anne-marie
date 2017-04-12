ba3bffed8a0f366bf908b5c5ae147f1808609f67
Katrine Mercure <katmercu@hotmail.com>
2017-04-12 13:03:02 -0400

    suite du gala distinction

--- src/www/travailfinal/wp-content/themes/ccistheme/page-gala.php
+++ src/www/travailfinal/wp-content/themes/ccistheme/page-gala.php
@@ -17,9 +17,24 @@
                             <div id="margedroite" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                                 <h3><?php the_title();?></h3>
 
-                                <!-- Rabais et privilèges -->
-                                <h4 class="espaceenahut">Devenir membre</h4>
-                                <p>Bacon ipsum dolor amet esse culpa deserunt picanha tail, ut sint kevin duis velit sunt. Est cupim do incididunt salami, ea tri-tip sed veniam magna culpa. Chicken meatloaf ut, excepteur nulla aliquip exercitation sunt tenderloin consectetur pork drumstick id tail. Short ribs aute ex porchetta burgdoggen pork chop et non ullamco laboris pork loin laborum. Shank rump laboris swine labore.</p>
+                                <!-- Historique -->
+                                <h4 class="espaceenahut">Historique</h4>
+                                <p>En collaboration avec d’importants partenaires économiques, le Gala Distinction Desjardins honore et reconnaît depuis plus de 20 ans les efforts des entrepreneurs de la région. Jusqu’à maintenant, pas moins de 250 entreprises se sont fait attribuer le titre de meilleure entreprise dans sa catégorie, ce qui représente près de 750 distinctions soulignées dans le milieu des affaires.</p>
+                                <p>Chaque année, le Gala rassemble au-delà de 400 personnes impliquées de près ou de loin dans milieu des affaires. Pour beaucoup, il s’agit d’un accès privilégié à cette communauté et d’un rayonnement incomparable pour l’entreprise.</p>
+                                <p>Le Gala Distinction Desjardins, c'est votre soirée !</p>
+
+                                <!-- Les avantages -->
+                                <h4 class="espaceenahut">Les avantages d'être finaliste</h4>
+                                <p>Être finaliste au Gala Distinction Desjardins est une superbe carte de visite. Il s’agit d’une  reconnaissance concrète des intervenants économiques de la région du succès de l'entreprise. Mais c'est aussi un bel ajout au CV corporatif.</p>
+                                <p>Depuis plus de 20 ans, la Chambre de commerce décerne ces prix aux entrepreneurs locaux. Ils sont donc nombreux à afficher fièrement le trophée reçu en reconnaissance de leurs efforts. Les succès de ces entrepreneurs sont reconnus chaque année devant plus de 400 personnalités d'affaires. Sans compter les clients et partenaires qui peuvent voir cette récompense lors de leur passage dans l'entreprise. Deux bonnes raisons de se dépasser chaque année et de présenter à nouveau, ou pour la première fois, sa candidature au Gala Distinction Desjardins.</p>
+                                <p>Jusqu’à maintenant, le Gala a permis à près d’une centaine d’entreprises de se faire connaître par un vaste public. Cela représente une occasion incroyable de réseauter et de se faire reconnaître par le milieu des affaires.</p>
+                                <p>Outre la visibilité offerte à même la soirée du Gala, vous profiterez d’une visibilité incomparable auprès des médias et du réseau de la Chambre. Ce placement médiatique équivaut à quelques milliers de dollars de publicité auprès de votre public cible.</p>
+                                <p>De plus, être finaliste apporte une grande crédibilité à l'entreprise. Cette visibilité médiatique est donc appelée à perdurer, puisque les médias se tourneront vers des entreprises crédibles pour leurs différents dossiers par exemple. Les clients et partenaires seront aussi séduits par cette valeur ajoutée.</p>
+                                <p>Être finaliste au Gala Distinction Desjardins apporte tant de retombées positives! Pourquoi attendre avant de déposer votre candidature pour la prochaine édition?</p>
+
+                                 <!-- Édition en cours -->
+                                <h4 class="espaceenahut">Gala Distinction Desjardins 2016</h4>
+                                <p>Découvrez nos 4 finalistes pour le nom du prix Personnalité d'affaires féminine. Le nom choisi sera dévoilé en juin 2017. Un merci tout spécial aux familles de nos finalistes et à Madeleine Lacoursière de la Société d'histoire et de généalogie de Shawinigan qui ont contribué à la recherche et à la rédaction des biographies. </p>
                             </div>
 
                             <!-- Section : Devenez membre -->
