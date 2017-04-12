152f29f4c7160f351e87aef27b98e4ddc0e93952
Katrine Mercure <katmercu@hotmail.com>
2017-04-12 12:46:05 -0400

    page le gala structure

--- src/www/travailfinal/wp-content/themes/ccistheme/page-gala.php
+++ src/www/travailfinal/wp-content/themes/ccistheme/page-gala.php
@@ -1,10 +1,39 @@
         <?php get_header();?>
+
+        <!-- Main -->
         <main>
             <!-- Image -->
             <div class="background-img hidden-xs" id="header">
-                <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/background-membre.png" alt="CCIS">
+                <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/background-membre.png" alt="L'espace membre">
                 <h1><?php the_title();?></h1>
             </div>
-            
-        </main>                
+
+            <!-- Section : Espace membre  -->
+            <section id="presentation-chambre" class="espace-membre">
+                <div class="container-fluid">
+                    <div class="row">
+
+                        <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
+                            <div id="margedroite" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                <h3><?php the_title();?></h3>
+
+                                <!-- Rabais et privilèges -->
+                                <h4 class="espaceenahut">Devenir membre</h4>
+                                <p>Bacon ipsum dolor amet esse culpa deserunt picanha tail, ut sint kevin duis velit sunt. Est cupim do incididunt salami, ea tri-tip sed veniam magna culpa. Chicken meatloaf ut, excepteur nulla aliquip exercitation sunt tenderloin consectetur pork drumstick id tail. Short ribs aute ex porchetta burgdoggen pork chop et non ullamco laboris pork loin laborum. Shank rump laboris swine labore.</p>
+                            </div>
+                        </div>    
+                        
+                        <!-- Sidebar -->
+                        <aside class="hidden-xs hidden-sm col-md-3 col-lg-3">
+                            <ul>
+                                <!-- Côté droit -->
+                                <?php if ( !function_exists('dynamic_sidebar')|| !dynamic_sidebar( 'sidebar-membre' ) ) : Endif; ?>
+                            </ul>
+                        </aside>
+                    </div>
+                </div>
+            </section>   
+        </main>
+        <!-- Fin du main -->
+
         <?php get_footer();?>
